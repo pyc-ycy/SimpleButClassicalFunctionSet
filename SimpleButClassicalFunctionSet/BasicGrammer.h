8c0f0b0ae5eb339fe8b8a7255101d9ec0742e4f4
7wc98#14 <2923616405@qq.com>
2020-11-16 21:54:55 +0800

    add content

--- SimpleButClassicalFunctionSet/BasicGrammer.h
+++ SimpleButClassicalFunctionSet/BasicGrammer.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
 // the follow is a function of print a string:"Hello word" by using different way.
@@ -7,9 +8,15 @@ void PrintHello(void)
 {
 	// manner one:
 	// Using the standardized input or output stream, which is provided by standardized library of C++
-	// Using this way, you must include the head file called iostream at top of the source file
+	// Using this way, you must include the head file called iostream.h at top of the source file
 	// and the "using namespce std" is a alternative, if you are not edit like me, you can use the format:
 	// std::cout<<something you want to output to screen
 	cout << "Hello world" << endl;
 	// A string is must be cover in ""
+	// manner two:
+	// print what you want to output in a way of using the printf function of C library
+	// before you start to use this function, you must include head file: stdio.h
+	printf("Hello world\n");
+	char s[] = "Hello world";
+	printf("%s", s);
 }
